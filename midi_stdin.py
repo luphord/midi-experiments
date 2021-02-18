a6dda56c1fa66c0ca16b1cedb37e6da359a9b525
luphord <luphord@protonmail.com>
2021-02-18 20:07:08 +0100

    make parsing more reliable

--- midi_stdin.py
+++ midi_stdin.py
@@ -2,9 +2,12 @@
 import mido
 import sys
 
+parser = mido.Parser()
+
 with mido.open_output("Synth input port (2986:0)") as out_port:
     while True:
-        msg_bytes = sys.stdin.buffer.read(3)
-        msg = mido.Message.from_bytes(msg_bytes)
-        print(msg)
-        out_port.send(msg)
+        msg_bytes = sys.stdin.buffer.read(1)
+        parser.feed(msg_bytes)
+        for msg in parser:
+            print(msg)
+            out_port.send(msg)
