52b7b535a7ba0e26dfbe6a682858b07d3152237a
luphord <luphord@protonmail.com>
2021-02-18 20:01:51 +0100

    write random midi messages to stdout

---
+++ midi_stdout.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+import mido
+import sys
+import time
+from random import randint
+
+while True:
+    msg = mido.Message(type="note_on", note=randint(60, 72), channel=2)
+    sys.stdout.buffer.write(bytes(msg.bytes()))
+    sys.stdout.buffer.flush()
+    time.sleep(1)
+    msg_off = mido.Message(type="note_off", note=msg.note, channel=msg.channel)
+    sys.stdout.buffer.write(bytes(msg_off.bytes()))
+    sys.stdout.buffer.flush()
+
