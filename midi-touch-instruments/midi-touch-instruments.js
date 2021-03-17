bb47f5d1d1f96f14da61de963865c565e231a5ed
luphord <luphord@protonmail.com>
2021-03-17 21:41:47 +0100

    create hex button grid

--- midi-touch-instruments/midi-touch-instruments.js
+++ midi-touch-instruments/midi-touch-instruments.js
@@ -20,8 +20,27 @@ var instruments = (function() {
         return hex;
     };
 
+    var create_hex_button_grid = (corner_center, radius, nrows, ncolumns, corner_note) => {
+        const buttons = [],
+            height = Math.sqrt(3) * radius;
+        for (let row = 0; row < nrows; row++) {
+            for (let col = 0; col < ncolumns; col++) {
+                const center = {
+                    x: corner_center.x + col * 1.5 * radius,
+                    y: corner_center.y + (row * height + (col % 2) * height / 2)
+                },
+                hex = create_hexagon(center, radius, 0, "fill:rgb(11, 99, 161);stroke:rgb(189, 183, 183);stroke-width:1");
+                hex.setAttribute("class", "button");
+                hex.setAttribute("data-note", corner_note - row * 7 + Math.floor(col / 2) - (col % 2) * 3);
+                buttons.push(hex);
+            }
+        }
+        return buttons;
+    }
+
     return {
         "hexagon_coordinates": hexagon_coordinates,
-        "create_hexagon": create_hexagon
+        "create_hexagon": create_hexagon,
+        "create_hex_button_grid": create_hex_button_grid
     };
 })();
